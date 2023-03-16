5c5301dd6fe977cdc43c133c5f22ac55919cf9aa
isieun0908 <supiya0908@naver.com>
2023-03-16 20:35:20 +0900

    feat: Question Entity 생성 (#1)

---
+++ classmate-backend-main/classmate/src/main/java/devteamOne/classmate/question/domain/Question.java
@@ -0,0 +1,30 @@
+package devteamOne.classmate.question.domain;
+
+import devteamOne.classmate.channel.domain.Channel;
+import devteamOne.classmate.user.domain.User;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Question {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String context;
+
+  private Boolean answered;
+
+  private Boolean pinned;
+
+  private LocalDateTime createdAt;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "channel_id")
+  private Channel channel;
+}
