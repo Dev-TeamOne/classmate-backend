5845bb57b4198735ab5e359b05338549443df94c
minseong <kdsvip5@naver.com>
2023-05-27 02:17:20 +0900

    feat: 로그인 기능 구현
    - Login 필터/서비스 구현
    - Login 성공/실패 핸들러 구현
    - JWT 설정 파일 추가

--- classmate/src/main/java/devteamOne/classmate/global/config/SecurityConfig.java
+++ classmate/src/main/java/devteamOne/classmate/global/config/SecurityConfig.java
@@ -1,17 +1,37 @@
 package devteamOne.classmate.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import devteamOne.classmate.global.jwt.JwtService;
+import devteamOne.classmate.global.jwt.filter.JwtAuthenticationProcessingFilter;
+import devteamOne.classmate.global.login.filter.CustomJsonAuthenticationFilter;
+import devteamOne.classmate.global.login.handler.LoginFailureHandler;
+import devteamOne.classmate.global.login.handler.LoginSuccessHandler;
+import devteamOne.classmate.global.login.service.LoginService;
+import devteamOne.classmate.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final ObjectMapper objectMapper;
+    private final UserRepository userRepository;
+    private final LoginService loginService;
+    private final JwtService jwtService;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,6 +49,50 @@ public class SecurityConfig {
                 .requestMatchers("/sign-up").permitAll()
                 .anyRequest().authenticated();
 
+        http.addFilterAfter(customJsonAuthenticationFilter(), LogoutFilter.class);
+        http.addFilterBefore(jwtAuthenticationProcessingFilter(), CustomJsonAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(loginService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() {
+        return new LoginSuccessHandler(jwtService, userRepository);
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
+    }
+
+
+    @Bean
+    public CustomJsonAuthenticationFilter customJsonAuthenticationFilter() {
+        CustomJsonAuthenticationFilter customJsonAuthenticationFilter =
+                new CustomJsonAuthenticationFilter(objectMapper);
+
+        customJsonAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        customJsonAuthenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
+        customJsonAuthenticationFilter.setAuthenticationFailureHandler(loginFailureHandler());
+
+        return customJsonAuthenticationFilter;
+    }
+
+    @Bean
+    public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
+        return new JwtAuthenticationProcessingFilter(jwtService, userRepository);
     }
 }
